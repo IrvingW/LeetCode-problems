dcd94455a8ef214e4c089b8d678dd32f3c2740f0
IrvingW <thor.wang@sjtu.edu.cn>
2018-05-16 13:36:40 +0800

    backtracking

---
+++ 040_Combination_Sum_II/sol.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void sum(vector<vector<int>> & result, int target, vector<int> & candidates, vector<int> prefix, int index){
+        if(target == 0){
+            result.push_back(prefix);
+            return;
+        }
+        if(target < candidates[0])  // can not reach 
+            return;
+        for(int i = index; i < candidates.size(); i++){
+            if(i != index && candidates[i] == candidates[i-1])
+                continue;
+            prefix.push_back(candidates[i]);
+            sum(result, target - candidates[i], candidates, prefix, i+1);
+            prefix.pop_back();
+
+        }
+    }
+       
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        sort(candidates.begin(), candidates.end());
+        if(target < candidates[0])  // not possible
+            return result;
+        vector<int> pre;
+        sum(result, target, candidates, pre, 0);
+        return result;   
+    }
+};
